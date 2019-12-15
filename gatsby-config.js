9d3171fe17eaa85ce6f689a02f281cf3517aef84
Fabian Terh <fabianterh@gmail.com>
2019-12-15 18:06:14 +0800

    Use gatsby-plugin-styled-components (optimize pageloads)

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,12 @@ module.exports = {
         head: true,
       },
     },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
