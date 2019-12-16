88e1c00ea6a5b4e54c7d0df66dec03cd2efd529f
Fabian Terh <fabianterh@gmail.com>
2019-12-16 11:46:44 +0800

    Tweak UI
    - new favicon
    - change theme-color

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,10 +21,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ff512f`,
+        theme_color: `#ff512f`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`,
       },
     },
     {
